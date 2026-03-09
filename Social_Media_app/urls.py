3a85b88bb4e5797150079660f036130c24fa69e0
Ahmad-Ali016 <mhrali016@gmail.com>
2026-03-09 20:23:11 +0000

    Fix email configuration using environment variables

--- Social_Media_app/urls.py
+++ Social_Media_app/urls.py
@@ -1,25 +1,11 @@
-"""
-URL configuration for Social_Media_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Social Media App is Live 🚀")),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/profiles/', include('profiles.urls')),
@@ -27,5 +13,3 @@ urlpatterns = [
     path('api/posts/', include('posts.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
